b92cf024f40f9a0844b83492845988b299533d80
Natu Myers <me@natumyers.com>
2016-03-02 01:22:29 -0500

    epilogue fail

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ var express        = require('express')
   , http           = require('http')
   , path           = require('path')
   , db             = require('./models')
+  , epilogue       = require('epilogue')
+  , http           = require('http')
+  , Sequelize      = require('sequelize')
+  , router         = require('express').Router(); // new
 
-var router =require('express').Router(); // new
+
 var app = express()
 
 // all environments
@@ -21,8 +25,8 @@ app.use(bodyParser())
 app.use(methodOverride())
 app.use(express.static(path.join(__dirname, 'public')))
 
-// MODELS // new
-var userVar     = require('./models/user');
+// IMPORT MODELS
+ var userVar     = require('./models/User');
 
 
 
@@ -33,6 +37,7 @@ if ('development' === app.get('env')) {
 
 
 
+// create db and listen
 db
   .sequelize
   .sync()
@@ -45,6 +50,25 @@ db
       })
     }
   })
+
+
+
+
+// Initialize epilogue
+epilogue.initialize({
+  app: app,
+  sequelize: db
+});
+
+// Create REST resource
+var userResource = epilogue.resource({
+  model: userVar,
+  endpoints: ['/users', '/users/:id']
+});
+
+
+
+
 
 
 
@@ -68,110 +92,4 @@ router.use(function(req, res, next) {
 
 
 
-// ROUTES FROM SEQUELIZE
-// =============================================================================
-
-
-/* USER ROUTES ENDING IN /users!
-router.route('/users')
-
-    // get all todos
-    .get( function(req, res) {
-      userVar.User.findAll({}).then(function(users) {
-        res.json(users);
-      });
-    })
-
-    .post(function(req, res) {
-        userVar.User.create({
-            email: req.body.email
-        }).then(function(user) {
-            res.json(user);
-            });
-    });
-
-*/
-
-module.exports = function(app) {
-  var apiRouter = express.Router();
-  apiRouter.get('/users', function(req, res, next){
-
-    res.json(
-
-        User.findAll({attributes: ['foo', 'bar'] })
-
-    );
-
-  });
-
-  return apiRouter;
-};
-
-
-// on routes that end in /users/:UserId
-// ----------------------------------------------------
-router.route('/users/:UserId')
-
-    // GET - get the user with that id (accessed at GET http://localhost:3000/api/users/:UserId)
-    .get(function(req, res) {
-        userVar.findById(req.params.UserId, function(err, user) {
-            if (err)
-                res.send(err);
-            res.json(user);
-        });
-    })
-
-    // PUT - update the user with this id (accessed at GET http://localhost:3000/api/users/:UserId)
-    .put(function(req, res) {
-
-        // use our user model to find the user we want
-        userVar.findById(req.params.UserId, function(err, user) {
-
-            if (err)
-                res.send(err);
-
-            User.name = req.body.username;  // update the users info
-
-            // save the user
-            User.save(function(err) {
-                if (err)
-                    res.send(err);
-
-                res.json({ message: 'User updated!' });
-            });
-
-        });
-    })
-
-    // DELETE - delete the user with this id (accessed at DELETE http://localhost:3000/api/users/:user_id)
-    .delete(function(req, res) {
-        userVar.remove({
-            _id: req.params.UserID
-        }, function(err, user) {
-            if (err)
-                res.send(err);
-
-            res.json({ message: 'User Successfully deleted' });
-        });
-    });
-
-
-
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
-
-
-
-
-
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-
-// ROUTES FOR OUR API
-// =============================================================================
 
